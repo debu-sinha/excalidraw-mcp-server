478c41d0b4aaa4e6720c126a6a7df93b2717afa2
debu-sinha <debusinha2009@gmail.com>
2026-02-07 18:27:03 -0500

    Add eslint flat config, fix lint scripts for Windows
    ESLint 9 needs eslint.config.js (flat config format).
    Also remove single-quoted globs from npm scripts since
    Windows cmd doesn't expand them.

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['src/**/*.ts', 'test/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', 'frontend/'],
+  },
+];
